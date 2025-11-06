5010a6d472aa88a8b3d500a27966aabf215297c1
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-06 12:30:29 +0100

    Done Ex3

--- BLOC1.PA02.Exam.MiquelManzano/Program.cs
+++ BLOC1.PA02.Exam.MiquelManzano/Program.cs
@@ -1,7 +1,40 @@
-﻿public class Program
+﻿using System.Data.SqlTypes;
+
+public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const string Spacer = "----------------------";
+        const string UserInputMsg = "Enter a number (0 to exit):";
+        const string UserErrorInputMsg = "Invalid input. Please enter a valid integer.";
+
+        bool validNumber;
+        int userNum;
+        int number;
+
+        do
+        {
+            Console.WriteLine(Spacer);
+            Console.WriteLine(UserInputMsg);
+            validNumber = int.TryParse(Console.ReadLine(), out userNum);
+            if (!validNumber)
+            {
+                Console.WriteLine(UserErrorInputMsg);
+            }
+            else
+            {
+                number = userNum;
+                Console.Write("The factors of {0} are: ", userNum);
+                while (number >= 1)
+                {
+                    if (userNum % number == 0)
+                    {
+                        Console.Write("{0} ", number);
+                    }
+                    number--;
+                }
+                Console.WriteLine();
+            }
+        } while (userNum != 0);
     }
 }
